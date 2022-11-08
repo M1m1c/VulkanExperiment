855042965a71ab756fa7c6333c734d7c08f81656
M1m1c <M1m1c@users.noreply.github.com>
2022-11-08 16:22:56 +0100

    Render Pipeline cont. RenderPass

--- main.cpp
+++ main.cpp
@@ -551,6 +551,39 @@ private:
 		}
 	}
 
+	void CreateRenderPass() 
+	{
+		VkAttachmentDescription colorAttachment{};
+		colorAttachment.format = m_SwapChainImageFormat;
+		colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+		colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+		VkAttachmentReference colorAttachmentRef{};
+		colorAttachmentRef.attachment = 0;
+		colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+		VkSubpassDescription subpass{};
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.colorAttachmentCount = 1;
+		subpass.pColorAttachments = &colorAttachmentRef;
+
+		VkRenderPassCreateInfo renderPassInfo{};
+		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+		renderPassInfo.attachmentCount = 1;
+		renderPassInfo.pAttachments = &colorAttachment;
+		renderPassInfo.subpassCount = 1;
+		renderPassInfo.pSubpasses = &subpass;
+
+		if (vkCreateRenderPass(m_Device, &renderPassInfo, nullptr, &m_RenderPass) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create render pass!");
+		}
+	}
+
 	void CreateGraphicsPipeline() 
 	{
 		auto vertShaderCode = readFile("shaders/vert.spv");
@@ -689,6 +722,7 @@ private:
 		CreateLogicalDevice();
 		CreateSwapChain();
 		CreateImageViews();
+		CreateRenderPass();
 		CreateGraphicsPipeline();
 	}
 
@@ -702,6 +736,11 @@ private:
 
 	void Cleanup()
 	{
+
+		vkDestroyPipelineLayout(m_Device, m_PipelineLayout, nullptr);
+
+		vkDestroyRenderPass(m_Device, m_RenderPass, nullptr);
+
 		vkDestroyPipelineLayout(m_Device, m_PipelineLayout, nullptr);
 
 		for (auto imageView : m_SwapChainImageViews) {
@@ -743,6 +782,7 @@ private:
 
 	std::vector<VkImageView> m_SwapChainImageViews;
 
+	VkRenderPass m_RenderPass;
 	VkPipelineLayout m_PipelineLayout;
 
 	const std::vector<const char*> m_DeviceExtensions = {
