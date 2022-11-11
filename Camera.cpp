a1b0af64cf84a4ca1b61f201041b4e722fda9b1b
M1m1c <M1m1c@users.noreply.github.com>
2022-11-11 16:19:46 +0100

    Added GetCamForward() and used it to clculate view matrix

--- Camera.cpp
+++ Camera.cpp
@@ -1,5 +1,14 @@
 #include "Camera.h"
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/gtc/matrix_transform.hpp>
+
+
+Camera::Camera()
+{
+	Position = vec3(0.f);
+	Yaw = 0.f;
+	Pitch = 0.f;
+}
 
 Camera::Camera(vec3 position, f32 yaw, f32 pitch) :
 	Position(position), Yaw(yaw), Pitch(pitch)
@@ -8,7 +17,24 @@ Camera::Camera(vec3 position, f32 yaw, f32 pitch) :
 
 mat4 Camera::CalcViewMatrix()
 {
-	return glm::lookAt(Position, vec3(cos(Yaw), cos(Pitch), sin(Yaw)), worldUp);
+	return glm::lookAtRH(Position, Position + GetCamForward(), worldUp);
+}
+
+vec3 Camera::GetCamForward()
+{
+	glm::vec3 dir;
+	dir.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+	dir.y = -sin(glm::radians(Pitch));
+	dir.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+	return glm::normalize(dir);
+}
+
+Projection::Projection()
+{
+	Aspect = 0.f;
+	Fov = 0.f;
+	Z_near = 0.f;
+	Z_far = 0.f;
 }
 
 Projection::Projection(u32 width, u32 height, f32 fov, f32 znear, f32 zfar) :
@@ -25,6 +51,10 @@ void Projection::Resize(u32 width, u32 height)
 mat4 Projection::CalcProjectionMatrix()
 {
 	return glm::perspective(glm::radians(Fov), Aspect, Z_near, Z_far);
+}
+
+CameraUniform::CameraUniform()
+{
 }
 
 CameraUniform::CameraUniform(Camera cam, Projection proj) :
--- Camera.h
+++ Camera.h
@@ -3,19 +3,24 @@
 
 using namespace glm;
 
-const  glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+const  glm::vec3 worldUp = glm::vec3(0.0f, 0.0f, 1.0f);
 
 struct Camera
 {
+public:
+	Camera();
 	Camera(vec3 position, f32 yaw, f32 pitch);
 	mat4 CalcViewMatrix();
+	vec3 GetCamForward();
 	vec3 Position;
 	f32 Yaw;
 	f32 Pitch;
 };
 
 struct Projection {
-	Projection(u32 width, u32 height, f32 fovy, f32 znear, f32 zfar);
+public:
+	Projection();
+	Projection(u32 width, u32 height, f32 fov, f32 znear, f32 zfar);
 	void Resize(u32 width, u32 height);
 	mat4 CalcProjectionMatrix();
 	f32 Aspect;
@@ -26,6 +31,8 @@ struct Projection {
 
 struct CameraUniform
 {
+public:
+	CameraUniform();
 	CameraUniform(Camera cam, Projection proj);
 	mat4 CalcMVP();
 	Camera Cam;
