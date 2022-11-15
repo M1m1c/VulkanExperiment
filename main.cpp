b1367ba30510c571fa0a8841645a9acb1cb83b82
M1m1c <M1m1c@users.noreply.github.com>
2022-11-15 15:55:27 +0100

    changed setting of light position to be based on scene light

--- main.cpp
+++ main.cpp
@@ -1356,8 +1356,8 @@ private:
 	{
 		static auto startTime = std::chrono::high_resolution_clock::now();
 
-		auto currentTime = std::chrono::high_resolution_clock::now();
-		float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+	/*	auto currentTime = std::chrono::high_resolution_clock::now();
+		float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();*/
 
 
 		UniformBufferObject ubo{};
@@ -1370,8 +1370,8 @@ private:
 
 		ubo.proj[1][1] *= -1;
 
-		auto rotate = glm::rotate(glm::mat4(1.0f), time * glm::radians(15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		ubo.lightPos = rotate[0];
+		//auto rotate = glm::rotate(glm::mat4(1.0f), time * glm::radians(15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		ubo.lightPos = m_CameraUniform.Light.GetLightPosition();
 
 		memcpy(m_UniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
 	}
